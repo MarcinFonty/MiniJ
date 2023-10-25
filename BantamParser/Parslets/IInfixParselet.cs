1adf79bbb91117329501e3c9631c09c597a3a055
School <School@DESKTOP-LCC8D49>
2023-10-25 02:42:22 +0200

    Added IInfixParselet

---
+++ BantamParser/Parslets/IInfixParselet.cs
@@ -0,0 +1,14 @@
+﻿using BantamParser.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BantamParser.Parslets
+{
+    internal interface IInfixParselet
+    {
+        IExpression Parse(Parser parser, IExpression left, Token token);
+    }
+}
