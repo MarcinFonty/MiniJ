2e08c431bc20bcd9657351929c5a1dcd4d1bdf37
School <School@DESKTOP-LCC8D49>
2023-10-26 18:39:21 +0200

    Added Postfix Parselet

---
+++ BantamParser/Expressions/PostfixExpression.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BantamParser.Expressions
+{
+    internal class PostfixExpression : IExpression
+    {
+        private IExpression mLeft { get; }
+        private TokenType mOperator { get; }
+        public PostfixExpression(IExpression left, TokenType operatoR)
+        {
+            mLeft = left;
+            mOperator = operatoR;
+        }
+
+        public void Print(StringBuilder sb)
+        {
+            sb.Append('(');
+            mLeft.Print(sb);
+            sb.Append(mOperator.Punctuator()).Append(')');
+        }
+    }
+}
---
+++ BantamParser/Parslets/PostfixOperatorParselet.cs
@@ -0,0 +1,17 @@
+﻿using BantamParser.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BantamParser.Parslets
+{
+    internal class PostfixOperatorParselet : IInfixParselet
+    {
+        public IExpression Parse(Parser parser, IExpression left, Token token)
+        {
+            return new PostfixExpression(left, token.mType);
+        }
+    }
+}
