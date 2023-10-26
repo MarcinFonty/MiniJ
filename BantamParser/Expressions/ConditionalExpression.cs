3341cdf7af250d781e84df676b41bbd94515faf5
School <School@DESKTOP-LCC8D49>
2023-10-26 19:36:48 +0200

    Conditional Parselet added

---
+++ BantamParser/Expressions/ConditionalExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BantamParser.Expressions
+{
+    internal class ConditionalExpression : IExpression
+    {
+        public IExpression mCondittion { get; }
+        public IExpression mThenArm { get; }
+        public IExpression mElseArm { get; }
+        public ConditionalExpression(IExpression condittion, IExpression thenArm, IExpression elseArm)
+        {
+            mCondittion = condittion;
+            mThenArm = thenArm;
+            mElseArm = elseArm;
+        }
+
+        public void Print(StringBuilder sb)
+        {
+            sb.Append("(");
+            mCondittion.Print(sb);
+            sb.Append(" ? ");
+            mThenArm.Print(sb);
+            sb.Append(" : ");
+            mElseArm.Print(sb);
+            sb.Append(")");
+        }
+    }
+}
---
+++ BantamParser/Parslets/ConditionalParselet.cs
@@ -0,0 +1,22 @@
+﻿using BantamParser.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BantamParser.Parslets
+{
+    internal class ConditionalParselet : IInfixParselet
+    {
+        public IExpression Parse(Parser parser, IExpression left, Token token)
+        {
+            IExpression thenArm = parser.ParseExpression();
+            parser.Consume(TokenType.COLON);
+            IExpression elseArm = parser.ParseExpression();
+
+            return new Expressions.ConditionalExpression(left, thenArm, elseArm);
+        }
+    }
+}
