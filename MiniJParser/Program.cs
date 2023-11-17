e41808c0af318d4638b9543d39f99ad52685a651
School <School@DESKTOP-LCC8D49>
2023-11-17 18:25:11 +0100

    Updated path to work dynamicaly in the project

--- MiniJParser/Program.cs
+++ MiniJParser/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string input = File.ReadAllText("C:\\Users\\marci\\Desktop\\Semester 7\\MiniJ\\ExampleMiniJ.js");
+        string input = File.ReadAllText("..\\..\\..\\..\\ExampleMiniJ.js"); //ugly
         //Lexer lexer = new Lexer("9 + 4 * 8 = 49");
         //Lexer lexer = new Lexer("A + C * D + 9 - 12");
         //Lexer lexer = new Lexer("-2 + -4");
