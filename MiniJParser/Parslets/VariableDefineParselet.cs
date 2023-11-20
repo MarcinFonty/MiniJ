63ae1d9690f923dd5391ae4e7a5668771dc5e69b
Marcinkiewicz,Marcin M <m.marcinkiewicz@student.fontys.nl>
2023-11-20 13:39:29 +0100

    Begin making parslet for variables (let)

---
+++ MiniJParser/Parslets/VariableDefineParselet.cs
@@ -0,0 +1,17 @@
+﻿using MiniJParser.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniJParser.Parslets
+{
+    internal class VariableDefineParselet : IPrefixParselet
+    {
+        public IExpression Parse(Parser parser, Token token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
