2770404e9876b276cbab659c23fb75ecc85c7528
School <School@DESKTOP-LCC8D49>
2023-11-21 00:50:20 +0100

    Added comment to clarify a problem I have

--- MiniJParser/Parslets/VariableDeclarationParselet.cs
+++ MiniJParser/Parslets/VariableDeclarationParselet.cs
@@ -9,12 +9,12 @@ namespace MiniJParser.Parslets
 {
     internal class VariableDeclarationParselet : IPrefixParselet
     {
-        public IExpression Parse(Parser parser, Token token) //TODO: Because of how Consume is used, it breaks ParseAllExpresions as it removes the semicolom.
+        public IExpression Parse(Parser parser, Token token) //TODO: Ask Herman, Because of how Consume is used, it breaks ParseAllExpresions as it removes the semicolom.
         {
             Token identifier = parser.Consume();
-            Token res = parser.Consume();
-            IExpression assignement = null;
-            if (res.Type == TokenType.ASSIGN)
+            Token temp = parser.Consume(); //Here's the problem with the todo above, could be resolved with using lookahead instead of Consume, but lookahead is private and not sure if I should be making it public. Gotta ask Herman.
+            IExpression assignement = null; //^Parser line 49 for where dirty fix is found^ <-- Parser ParseAllExpressions lookahead stuff I do there.
+            if (temp.Type == TokenType.ASSIGN)
             {
                 assignement = parser.ParseExpression();
             }
