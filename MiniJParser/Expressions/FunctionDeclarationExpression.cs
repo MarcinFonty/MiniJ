1c771c58d06733b4a0a67d5015a2bf2204a2e046
Marcinkiewicz,Marcin M <m.marcinkiewicz@student.fontys.nl>
2023-12-01 10:48:59 +0100

    Made FunctionDeclarationExpression

--- MiniJParser/Expressions/FunctionDeclarationExpression.cs
+++ MiniJParser/Expressions/FunctionDeclarationExpression.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MiniJParser.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,17 +11,33 @@ namespace MiniJParser.Expressions
     internal class FunctionDeclarationExpression : IExpression
     {
         private string _name;
-        private List<IExpression> _arguments;
+        private List<IExpression> _parameterExpressions;
         private IExpression _blockExpressions;
-        public FunctionDeclarationExpression(string name, List<IExpression> arguments, IExpression blockExpressions)
+        public FunctionDeclarationExpression(string name, List<IExpression> parameterExpressions, IExpression blockExpressions)
         {
             _name = name;
-            _arguments = arguments;
+            _parameterExpressions = parameterExpressions;
             _blockExpressions = blockExpressions;
         }
         public void Print(StringBuilder sb, string indent = "")
         {
-            throw new NotImplementedException();
+            sb.AppendLine("FunctionDeclarationExpression");
+
+            sb.Append(indent).Append("- name " + _name);
+            sb.AppendLine();
+
+            foreach (IExpression parameter in _parameterExpressions)
+            {
+                sb.Append(indent).Append("- parameter ");
+                parameter.Print(sb, indent + "\t");
+                sb.AppendLine();
+            }
+
+            sb.Append(indent).Append("- BlockExpression");
+            sb.AppendLine();
+            StringBuilder helperBuilder = new StringBuilder();
+            _blockExpressions.Print(helperBuilder, indent + "\t");
+            sb.AppendIndented(helperBuilder.ToString(), indent);
         }
     }
 }
